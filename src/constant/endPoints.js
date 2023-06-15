2cddf7a6067ca20607c61f39174ae7d3a22dfd02
vijayg963TG <121848023+vijayg963TG@users.noreply.github.com>
2023-06-15 11:53:08 +0530

    Update endPoints.js

--- src/constant/endPoints.js
+++ src/constant/endPoints.js
@@ -1,11 +1,11 @@
 const endPoints = {
-  create: '/v1/create',
-  login: '/v1/login',
-  logout: '/v1/logout',
-  sendMail: '/v1/sendmail',
-  resetPassword: '/v1/reset-password/?token=',
-  changePassword: '/v1/change-password/',
-  recordAttendance: '/v1/record-attendance'
+  create: '/create',
+  login: '/login',
+  logout: '/logout',
+  sendMail: '/sendmail',
+  resetPassword: '/reset-password/?token=',
+  changePassword: '/change-password/',
+  recordAttendance: '/record-attendance'
 };
 
 export default endPoints;
